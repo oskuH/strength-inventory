26ea1973cdfdaf10ccff80efe31e2404e5f5413c
Oskari <104070288+oskuH@users.noreply.github.com>
2026-01-23 14:46:00 +0200

    Migrate session model

---
+++ backend/src/migrations/20260123_00_initialise_sessions.ts
@@ -0,0 +1,29 @@
+import { DataTypes } from 'sequelize';
+import { type Migration } from '../utils/db.ts';
+//.ts instead of .js to accommodate Vitest
+
+const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.createTable('sessions', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true
+    },
+    user_id: {
+      type: DataTypes.STRING, // migrate to Sequalize UUID
+      allowNull: false,
+      references: { model: 'users', key: 'id' }
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE
+  });
+};
+
+const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.dropTable('sessions');
+};
+
+export { up, down };
